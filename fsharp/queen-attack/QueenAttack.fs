99999677009f7c3267fafe5d3884793602c67a05
Alex <macaronisupermoon@gmail.com>
2020-02-08 09:48:12 -0600

    the solution i plan to implement

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -2,4 +2,10 @@
 
 let create (position: int * int) = failwith "You need to implement this function."
 
+//if the x value is the same, they can attack
+//if the y value is the same, they can attack
+//if -x,+y converge, they can attack
+//if -x, -y converge, they can attack
+//if +x, +y converge, they can attack
+//if +x, -y converge, they can attack
 let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
